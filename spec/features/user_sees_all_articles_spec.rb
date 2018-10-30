8af311d3b50f5c2111366463e4e30b009b8b26e0
rcm32000 <rcm32000@yahoo.com>
2018-10-30 17:33:50 -0600

    Passing link tests

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -8,8 +8,8 @@ describe 'user sees all articles' do
 
       visit '/articles'
 
-      expect(page).to have_content(article1.title)
-      expect(page).to have_content(article2.title)
+      expect(page).to have_link(article1.title)
+      expect(page).to have_link(article2.title)
     end
   end
 end
